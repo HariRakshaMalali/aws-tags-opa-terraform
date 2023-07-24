3560e4e694e6d2c9f38b590d2c2bb1dc92a76091
root <root@bti-43nqdhu94weotjotsqqhw.us-west4-c.c.boreal-rain-289205.internal>
2023-07-24 10:53:43 +0000

    Updated  S3 bucket value in provider.tf file

--- provider.tf
+++ provider.tf
@@ -11,7 +11,7 @@ terraform {
   required_version = ">= 0.14.0"
 
   backend "s3" {
-    bucket = "ase-terraform-state-bucket"
+    bucket = "2a62ebed-5ac2-4333-a3e5-98bd7cfe017c-tfstate"
     key    = "terraform.tfstate"
     region = "us-west-2"
   }
